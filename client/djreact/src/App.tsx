9a207f83c725f08653b08cba091c5495b5eb663c
Marco Seo <marco.dseo@gmail.com>
2020-11-10 22:49:45 -0500

    removed submodule on react app

---
+++ client/djreact/src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import { Layout } from './components/Layout/Layout';
+
+function App() {
+  return (
+    <div className='App'>
+      <Layout>asdf</Layout>
+    </div>
+  );
+}
+
+export default App;
